af53c24787ab776bf6c4f89985f34f9cc8acf869
Saurabh Shukla <saurabh1258.be21@chitkarauniversity.edu.in>
2024-01-01 17:58:43 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [], // Make sure 'js-cookie' is not listed here
+    },
+  },
 })
